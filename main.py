42b3bf64646feb82a57c1aa1e68e2efa7c325579
insideggg <havrvit@gmail.com>
2024-10-06 16:22:55 +0200

    release 1.03

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if response.status_code == 200:
     # skip first two headers ('Nr', 'Godz')
     headers = [header.get_text() for header in table.find_all('th')[2:]]
 
-    rows = table.find_all('tr')[1:]  # Skip the first header row
+    rows = table.find_all('tr')[1:]  # Skip the first header row and start reading from "1. 7:30-8:15..."
 
     timetable = {day: [] for day in headers}
     for row in rows:
@@ -24,19 +24,43 @@ if response.status_code == 200:
         time = cells[1].get_text()
 
         for i, day in enumerate(headers):
+            # i+2 because first subject cell starts from 3 column
+            class_info = cells[i + 2].get_text(strip=True)
+
+            class_name_N = ''
+            class_name_P = ''
+
+            if "-n" in class_info and "-p" in class_info:
+                class_parts = class_info.split("-n", 1)
+                class_name_N = class_parts[0] + "-n"
+                class_name_P = class_parts[1].split("-p", 1)[0] + "-p" if "-p" in class_parts[1] else ''
+            elif "-n" in class_info and "-p" not in class_info:
+                # lesson is only in (N) week
+                class_name_N = class_info
+            elif "-n" not in class_info and "-p" in class_info:
+                # lesson is only in (P) week
+                class_name_P = class_info
+            elif "-n" not in class_info and "-p" not in class_info:
+                class_name_N = class_info
+                class_name_P = class_info
+
             timetable[day].append({
                 'period': period,
                 'time': time,
-                'class': cells[i + 2].get_text(strip=True) # i+2 because first subject cell starts from 3 column
+                'class_name_N': class_name_N.strip(),
+                'class_name_P': class_name_P.strip() # if class_name_P else class_name_N.strip()
             })
 
     with open('timetable.txt', 'w', encoding='utf-8') as f:
         for day, schedule in timetable.items():
             f.write(f"{day}:\n")
             for entry in schedule:
-                f.write(f"Period: {entry['period']} Time: {entry['time']}: Class: {entry['class']}\n")
+                f.write(f"Period: {entry['period']} Time: {entry['time']}: Class (N): {entry['class_name_N']} Class (P): {entry['class_name_P']}\n")
             f.write("\n")
     print("Schedule has been successfully written to timetable.txt!")
 else:
     print(f"Failed to retrieve the page. Status code: {response.status_code}")
+
+
+
 
